10b682bbbccd3abccf0d35c2d9d1f340a9c6d536
freekmurze <freekmurze@users.noreply.github.com>
2023-05-16 11:14:53 +0000

    Fix styling

--- src/Prometheus.php
+++ src/Prometheus.php
@@ -3,7 +3,6 @@
 namespace Spatie\Prometheus;
 
 use Spatie\Prometheus\Actions\RenderCollectorsAction;
-use Spatie\Prometheus\MetricTypes\Metric;
 use Spatie\Prometheus\MetricTypes\Gauge;
 use Spatie\Prometheus\MetricTypes\MetricType;
 
