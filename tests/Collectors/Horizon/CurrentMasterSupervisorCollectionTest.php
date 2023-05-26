56f8b7e29cdc47a5269199de45d7fd1328eea660
freekmurze <freekmurze@users.noreply.github.com>
2023-05-26 07:19:12 +0000

    Fix styling

--- tests/Collectors/Horizon/CurrentMasterSupervisorCollectionTest.php
+++ tests/Collectors/Horizon/CurrentMasterSupervisorCollectionTest.php
@@ -1,10 +1,9 @@
 <?php
 
 use Spatie\Prometheus\Collectors\Horizon\CurrentMasterSupervisorCollector;
-use Spatie\Prometheus\Collectors\Horizon\CurrentWorkloadCollector;
 
-it('can register the current master supervisor collector', function() {
-   app(CurrentMasterSupervisorCollector::class)->register();
+it('can register the current master supervisor collector', function () {
+    app(CurrentMasterSupervisorCollector::class)->register();
 
     assertPrometheusResultsMatchesSnapshot();
 });
