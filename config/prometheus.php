7068074130b0c102013f2aca5142a8f37b76ff1f
Freek Van der Herten <freek@spatie.be>
2023-05-31 08:41:46 +0200

    wip

--- config/prometheus.php
+++ config/prometheus.php
@@ -11,20 +11,24 @@ return [
     ],
 
     /*
-     * The middleware that will be applied to the url above
+     * The middleware that will be applied to the urls above
      */
     'middleware' => [
         Spatie\Prometheus\Http\Middleware\AllowIps::class,
     ],
 
     /*
-     * Only these IP's will be allowed to see the metrics.
+     * Only these IP's will be allowed to visit the above urls.
      * When set to `null` all IP's are allowed.
      */
     'allowed_ips' => [
 
     ],
 
+    /*
+     * You can override these classes to customize low-level behaviour of the package.
+     * In most cases, you can just use the defaults.
+     */
     'actions' => [
         'render_collectors' => Spatie\Prometheus\Actions\RenderCollectorsAction::class,
     ],
