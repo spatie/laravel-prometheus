e9b4ecf6440fa5eebb9ca905e16b400efddddafa
Max Carvalho <maaxxicarvalho@gmail.com>
2023-06-28 10:56:10 +0100

    feat: Improves code completion by class annotation

--- src/Facades/Prometheus.php
+++ src/Facades/Prometheus.php
@@ -3,9 +3,15 @@
 namespace Spatie\Prometheus\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Spatie\Prometheus\MetricTypes\MetricType;
 
 /**
  * @see \Spatie\Prometheus\Prometheus
+ *
+ * @method static \Spatie\Prometheus\MetricTypes\Gauge addGauge(string $name)
+ * @method static \Spatie\Prometheus\Prometheus registerCollector(MetricType $collector)
+ * @method static \Spatie\Prometheus\Prometheus registerCollectorClasses(array $collectors)
+ * @method static string renderCollectors(string $urlName = 'default')
  */
 class Prometheus extends Facade
 {
