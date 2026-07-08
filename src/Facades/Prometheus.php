29c002d7a4ce808e46866a26b2dfd14013821f1f
Cédric Neukom <cen@cyon.ch>
2026-07-08 13:17:22 +0200

    Update @method annotations in Prometheus-facade.

--- src/Facades/Prometheus.php
+++ src/Facades/Prometheus.php
@@ -8,10 +8,10 @@ use Spatie\Prometheus\MetricTypes\MetricType;
 /**
  * @see \Spatie\Prometheus\Prometheus
  *
- * @method static \Spatie\Prometheus\MetricTypes\Gauge addGauge(string $name)
- * @method static \Spatie\Prometheus\MetricTypes\Counter addCounter(string $name)
- * @method static \Spatie\Prometheus\Prometheus registerCollector(MetricType $collector)
- * @method static \Spatie\Prometheus\Prometheus registerCollectorClasses(array $collectors)
+ * @method static \Spatie\Prometheus\MetricTypes\Gauge addGauge(string $label, float|callable|null $value = null, ?string $name = null, ?string $namespace = null, ?string $helpText = null)
+ * @method static \Spatie\Prometheus\MetricTypes\Counter addCounter(string $label, int|callable|null $initialValue = null, ?string $name = null, ?string $namespace = null, ?string $helpText = null)
+ * @method static self registerCollector(MetricType $collector)
+ * @method static self registerCollectorClasses(array $collectors, array $constructorParameters = [])
  * @method static string renderCollectors(string $urlName = 'default')
  */
 class Prometheus extends Facade
